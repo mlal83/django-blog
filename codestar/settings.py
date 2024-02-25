48de5f0ee49cc5dab879ebe8851f5a5f3714fb8b
Manjula Lal <manjula_lal@hotmail.com>
2024-02-25 16:01:12 +0000

    connected with elephantsql

--- codestar/settings.py
+++ codestar/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gkkl0#^&@40_$(^4zk#y81v-cey$1ts6xzq4g6%-dw*)phh1)j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-mlal83-djangoblog-yv727ift0uq.ws-eu108.gitpod.io', '.herokuapp.com']
 
@@ -73,11 +78,15 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL", "postgres://rchvisye:w3GRZdn3YbsUTtqAC6ZWTR1ZVZSTzihz@trumpet.db.elephantsql.com/rchvisye"))
 }
 
 
