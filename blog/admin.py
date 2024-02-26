553e2d957cebc4d1d1760a2afc210375b0853918
Manjula Lal <manjula_lal@hotmail.com>
2024-02-26 19:19:48 +0000

    testing11

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+from .models import Comment
 
 from .models import Post
 
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Comment)
+
+
+
+
